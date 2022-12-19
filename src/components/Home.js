b908680d5c9da197ce4a480f65ff18baca864088
Imran <imrantipu90@yahoo.com>
2022-12-19 07:00:53 +0100

    categories shown in home page with start quiz button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import "./Home.css"
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Categories from "./Categories";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className=" bg">
+    const quizs = useLoaderData();
+  const newQuizes = quizs.data;
+  return (
+    <div>
+      <div className=" bg">
         <div className="content">
           <p>
-            This site is test your skill on different types of tech languages,such
-            as reactjs, javaScript,CSS, git.Our main motive to test your knowledge
-            specific on topics.
+            This site is test your skill on different types of tech
+            languages,such as reactjs, javaScript,CSS, git.Our main motive to
+            test your knowledge specific on topics.
           </p>
         </div>
       </div>
-    );
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {newQuizes.map((quiz) => (
+            <Categories key={quiz.id} quiz={quiz}></Categories>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
