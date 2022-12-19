8f456e8fa3547cc461248f0081f00100668dc8f2
Imran <imrantipu90@yahoo.com>
2022-12-19 12:11:08 +0100

    toaster addend to show right or wrong answer in question page

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,19 @@
 import React from "react";
+// import { toast, Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 
 const Quiz = (props) => {
   const { q, index } = props;
   const { question, options } = q;
+  const onChangeValue = (event) => {
+    const targetValue = event.target.value;
+    const val = q.correctAnswer;
+    if (targetValue === val) {
+      toast.success("Correct Answer");
+    } else {
+      toast.error("Wrong Answer");
+    }
+  };
   return (
     <div className="card mt-5 w-4/5 bg-primary text-primary-content mx-auto h-auto">
       <div className="card-body">
@@ -10,32 +21,40 @@ const Quiz = (props) => {
           Quiz {index + 1}: {question}
         </h2>
         <div className="form-control">
-          <div>
+          <div onChange={onChangeValue}>
             <label className="flex justify-items-center mt-3">
               <input type="radio" value={options[0]} name="radio-7" className="radio radio-info" />
               <span className="label-text ml-3">{options[0]}</span>
             </label>
           </div>
-          <div>
+          </div>
+          <div className="form-control">
+          <div onChange={onChangeValue}>
             <label className="flex justify-items-center mt-3">
               <input type="radio" value={options[1]} name="radio-7" className="radio radio-info" />
               <span className="label-text ml-3">{options[1]}</span>
             </label>
           </div>
-          <div>
+          </div>
+          <div className="form-control">
+          <div onChange={onChangeValue}>
             <label className="flex justify-items-center mt-3">
               <input type="radio" value={options[2]} name="radio-7" className="radio radio-info" />
               <span className="label-text ml-3">{options[2]}</span>
             </label>
           </div>
-          <div>
-            <label className="flex justify-items-center mt-3">
+          </div>
+          <div className="form-control">
+          <div onChange={onChangeValue}>
+            <label  className="flex justify-items-center mt-3">
               <input type="radio" value={options[3]}  name="radio-7" className="radio radio-info" />
               <span className="label-text ml-3">{options[3]}</span>
             </label>
           </div>
-        </div>
+          </div>
+        
       </div>
+      <Toaster />
     </div>
   );
 };
